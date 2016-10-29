3f6c6203e010f42045f702a1e960e20d8b49ad13
Przemysław Łapiński <przemyslaw.lapinski.8498@gmail.com>
2016-10-29 15:49:54 +0200

    default vue example

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>App</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+        
+    </head>
+    <body>
+        <div id="app" class="content">
+        @yield('content')
+        </div>
+        <!-- Scripts -->
+        <script src="/js/app.js"></script>
+    </body>
+</html>
