4f20518179bd8662c169aa130317c92ab34c3245
Przemysław Łapiński <przemyslaw.lapinski.8498@gmail.com>
2016-11-11 11:48:25 +0100

    create administration module

---
+++ laravel/app/Modules/Administration/Providers/ModuleServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Administration\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'administration');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'administration');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
